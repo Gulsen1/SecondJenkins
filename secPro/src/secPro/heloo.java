c3ad26b6724cb13c0369a9b177ab4f296086d261
gulsenturhan <gulsenturhan@192.168.0.2>
2020-08-02 13:45:07 -0500

    added

--- secPro/src/secPro/heloo.java
+++ secPro/src/secPro/heloo.java
@@ -4,6 +4,7 @@ public class heloo {
 	public static void main(String[]args) {
 		System.out.println("hello");
 		System.out.println("hello");
+		System.out.println("hello");
 		//test comment
 		//how are you?
 	}
