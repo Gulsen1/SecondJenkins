266abaad8301a0d763d5c6c32d1f81a2eda9aa34
gulsenturhan <gulsenturhan@172.20.10.4>
2020-08-02 20:07:00 -0500

    added line

--- thirdP/src/thirdP/hi.java
+++ thirdP/src/thirdP/hi.java
@@ -5,10 +5,7 @@ public class hi {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("hello");
-		System.out.println("hello");
-		//test comment
-		//how are you?
-		//how are you?
+		
 	}
 
 }
